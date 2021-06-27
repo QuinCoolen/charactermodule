2c76bbc770d0b066e3362e48c310789bd13499fc
QuinCoolen <quin.coolen@gmail.com>
2021-06-27 17:58:07 +0200

    small fix

--- sh_charactermodule_names.lua
+++ sh_charactermodule_names.lua
@@ -1,6 +1,14 @@
 local plyMeta = FindMetaTable("Player")
 
+function plyMeta:Nick()
+    return self:GetNWString("Nickname", false) or self:Nick()
+end
+
 function plyMeta:GetName()
+    return self:GetNWString("Nickname", false) or self:Nick()
+end
+
+function plyMeta:Name()
     return self:GetNWString("Nickname", false) or self:Nick()
 end
 
@@ -14,4 +22,20 @@ end
 
 function plyMeta:SetDesc(desc)
     self:SetNWString("Description", desc)
+end
+
+-- Shamelessly stolen from https://steamcommunity.com/sharedfiles/filedetails/?id=659490574
+if CLIENT then
+	function parseChat(ply, msg)
+		chat.AddText(Color(255, 255, 255, 255), plyMeta:GetName(), color_white, ": ", msg )
+		chat.PlaySound()
+		return true
+	end
+	timer.Simple( 5, function()
+		
+		if hook.GetTable()["OnPlayerChat"] then
+		else
+			hook.Add( "OnPlayerChat", "NickName_ChatFix", parseChat )
+		end
+	end)
 end
