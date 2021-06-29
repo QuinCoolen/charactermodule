850c01434640defb577352583b555529caf2cecf
Quin Coolen <quin.coolen@gmail.com>
2021-06-29 10:15:50 +0200

    Still doesn't work, made progress

--- sh_charactermodule_names.lua
+++ sh_charactermodule_names.lua
@@ -4,14 +4,6 @@ function plyMeta:Nick()
     return self:GetNWString("Nickname", false) or self:Nick()
 end
 
-function plyMeta:GetName()
-    return self:GetNWString("Nickname", false) or self:Nick()
-end
-
-function plyMeta:Name()
-    return self:GetNWString("Nickname", false) or self:Nick()
-end
-
 function plyMeta:SetName(name)
     self:SetNWString("Nickname", name)
 end
@@ -27,7 +19,7 @@ end
 -- Shamelessly stolen from https://steamcommunity.com/sharedfiles/filedetails/?id=659490574
 if CLIENT then
 	function parseChat(ply, msg)
-		chat.AddText(Color(255, 255, 255, 255), plyMeta:Nick(), color_white, ": ", msg )
+		chat.AddText(Color(255, 255, 255, 255), ply, color_white, ": ", msg )
 		chat.PlaySound()
 		return true
 	end
